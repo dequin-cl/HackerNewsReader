185adb99fd381e96380342f96268799495c78c61
Iván GalazJeria <dequin@gmail.com>
2020-11-27 10:56:48 -0300

    Add Hit creation from Data Transfer Object

---
+++ HackerNewsReader/Data/Hit.swift
@@ -0,0 +1,41 @@
+//
+//  Hit.swift
+//  HackerNewsReader
+//
+//  Created by Iván on 27-11-20.
+//
+
+import Foundation
+import CoreData
+
+extension Hit {
+    
+    /// Given a Hit DTO, this functions creates a Hit Core Data Object
+    /// ~~~
+    /// Hit.from(dto: hitDTO, in: viewContext)
+    /// ~~~
+    /// - Warning: This function will save on the Core Data context immediately
+    /// - Parameters:
+    ///   - dto: The Data Transfer Object obtained from the JSON file or network
+    ///   - context: Core Data context to save the object
+    /// - Returns: A Hit Core Data Managed Objects
+    static func from(dto: HitDTO, in context: NSManagedObjectContext) -> Hit {
+        let hit = Hit(context: context)
+        
+        hit.author = dto.author
+        hit.createdAt = dto.createdAt
+        hit.id = dto.objectID
+        hit.storyTitle = dto.storyTitle
+        hit.title = dto.title
+        
+        context.perform {
+          do {
+            try context.save()
+          } catch let error as NSError {
+            fatalError("Unresolved error \(error), \(error.userInfo)")
+          }
+        }
+        
+        return hit
+    }
+}
---
+++ HackerNewsReaderTests/Data/HitCoreDataTests.swift
@@ -0,0 +1,87 @@
+//
+//  HitCoreDataTests.swift
+//  HackerNewsReaderTests
+//
+//  Created by Iván on 27-11-20.
+//
+
+import XCTest
+@testable import HackerNewsReader
+
+class HitCoreDataTests: XCTestCase {
+    
+    func testInitializeFromDTO() throws {
+        /// Given
+        let bundle = Bundle(for: type(of: self))
+        guard let url = bundle.url(forResource: "SampleNews", withExtension: "json") else {
+            XCTFail("Missing file: SampleNews.json")
+            return
+        }
+        
+        let json = try Data(contentsOf: url)
+        let hitDTO = try HitDTO(data: json)
+        let viewContext = PersistenceController.emptyPreview.container.viewContext
+        
+        expectation(
+            forNotification: .NSManagedObjectContextDidSave,
+            object: viewContext) { _ in
+            return true
+        }
+        
+        /// When
+        viewContext.perform {
+            let hitCD = Hit.from(dto: hitDTO, in: viewContext)
+            /// Then
+            XCTAssertNotNil(hitCD, "The Hit should be created on Core Data")
+            XCTAssertEqual(hitCD.author, hitDTO.author)
+            XCTAssertEqual(hitCD.createdAt, hitDTO.createdAt)
+            XCTAssertEqual(hitCD.id, hitDTO.objectID)
+            XCTAssertEqual(hitCD.storyTitle, hitDTO.storyTitle)
+            XCTAssertEqual(hitCD.title, hitDTO.title)
+            
+            XCTAssertFalse(hitCD.isUserDeleted)
+        }
+        
+        waitForExpectations(timeout: 2.0) { error in
+            XCTAssertNil(error, "Save did not occur")
+        }
+        
+    }
+    
+    func testInitializeFromDTO2() throws {
+        /// Given
+        let bundle = Bundle(for: type(of: self))
+        guard let url = bundle.url(forResource: "SampleNews2", withExtension: "json") else {
+            XCTFail("Missing file: SampleNews2.json")
+            return
+        }
+        
+        let json = try Data(contentsOf: url)
+        let hitDTO = try HitDTO(data: json)
+        let viewContext = PersistenceController.emptyPreview.container.viewContext
+        
+        expectation(
+            forNotification: .NSManagedObjectContextDidSave,
+            object: viewContext) { _ in
+            return true
+        }
+        
+        /// When
+        viewContext.perform {
+            let hitCD = Hit.from(dto: hitDTO, in: viewContext)
+            /// Then
+            XCTAssertNotNil(hitCD, "The Hit should be created on Core Data")
+            XCTAssertEqual(hitCD.author, hitDTO.author)
+            XCTAssertEqual(hitCD.createdAt, hitDTO.createdAt)
+            XCTAssertEqual(hitCD.id, hitDTO.objectID)
+            XCTAssertEqual(hitCD.storyTitle, hitDTO.storyTitle)
+            XCTAssertEqual(hitCD.title, hitDTO.title)
+            
+            XCTAssertFalse(hitCD.isUserDeleted)
+        }
+        
+        waitForExpectations(timeout: 2.0) { error in
+            XCTAssertNil(error, "Save did not occur")
+        }
+    }
+}
